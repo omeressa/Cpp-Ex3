ee1a4bb11855b573bb6408fdd793d1e0b4b7bc69
OmarEssa <33825687+omeressa@users.noreply.github.com>
2018-04-11 07:16:06 +0300

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,49 @@
+
+/*#include <iostream>
+using namespace std;
+#include "Member.h"
+
+bool test(int a,int b){
+  if(a==b)return true;
+  return false;
+}
+
+
+
+/*Testing the changes of the number of all users
+
+void test2() {
+  Member a;
+  cout<<"Number of all users is :"<< Member::count()<<endl;
+  Member b;
+  cout<<"Number of all users is :"<< Member::count()<<endl;
+  Member c;
+  cout<<"Number of all users is :"<< Member::count()<<endl;
+}
+/*Testing the follow functiong , following and followers
+void test3() {
+  Member a,b,c;
+  int ans = a.numFollowing();
+  cout<<"is a following one person? (true = 1, false = 0) :"<< test(ans,3)<<endl;
+  a.follow(b);
+  cout<< "is a following one person? (true = 1, false = 0) :"<<test(a.numFollowing(),1)<<endl;
+  a.follow(c);
+  cout<<"is a following two persons? (true = 1, false = 0) :"<<test(a.numFollowing(),2)<<endl<<endl;
+  cout<<"number of members following a: "<<a.numFollowers()<<endl;
+  cout<<"number of members following b: "<<b.numFollowers()<<endl;
+  cout<<"number of members following c: "<<c.numFollowers()<<endl<<endl;
+  cout<<"what happens after a unfollowing both b,c :"<<endl;
+  a.unfollow(b);
+  a.unfollow(c);
+  cout<< "is a following one person? (true = 1, false = 0) :"<<test(a.numFollowing(),1)<<endl;
+  cout<<"is a following two persons? (true = 1, false = 0) :"<<test(a.numFollowing(),2)<<endl<<endl;
+    cout<<"number of members following b: "<<b.numFollowers()<<endl;
+  cout<<"number of members following c: "<<c.numFollowers()<<endl<<endl;
+}
+int main() {
+  	  cout<<"Testing the changes of the number of all users :"<<endl;
+  test2();
+  cout<<"Testing the follow functiong , following and followers :"<<endl;
+  test3();
+}
+*/
