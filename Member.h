84f05677c151e920c9b9bc11a9b819ba3f4d4179
OmarEssa <33825687+omeressa@users.noreply.github.com>
2018-04-21 12:45:09 +0300

    Update Member.h

--- Member.h
+++ Member.h
@@ -7,29 +7,20 @@
 
 class Member {
 	
-	static int AllUsers;// total number of members
+	static int AllMembers; // total number of members 
+	std::set<Member*> followers; // members which I'm following
+	std::set<Member*> following; // members which are following me
 	
-	/**************************************************************************************/
-
-	std::set<Member*> followers;// members which I'm following
-	std::set<Member*> following;// members which are following me
-	
-	/**************************************************************************************/
-             /*both adding and deleting functions r to ease the insert and erase procsess*/
-	
+        /*****both adding and deleting functions r to ease the insert and erase procsess*****/
 	void adding(Member& other);//Add to following
 	void deleting(Member& other);//Remove from following
 	
-	/**************************************************************************************/
 
 public:
 	Member(); // This is the constructor declaration
 	~Member(); // This is the destructor declaration
-	/**************************************************************************************/
 	
 	static int count();//gives back the number of all members
-	
-	/**************************************************************************************/
 
 	void follow(Member& o);//function to follow a member
 	void unfollow(Member& o);//function to unfollow a member
